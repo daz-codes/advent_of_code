0f730ba76764cca18223a41cd4b81ce61a0ba97b
DAZ <daz4126@gmail.com>
2025-12-01 16:55:05 +0000

    Update day1.js

--- 2025/day1.js
+++ 2025/day1.js
@@ -13,15 +13,14 @@ R14
 L82
 `
 
-
 const day1 = data => {
   const instructions = data.trim().split("\n")
   let dial_value = 50
 
-  const part_turn_goes_past_zero = (amount_turned,dir) =>
-      dial_value.isNonzero && dir.eql("L")
+  const extra_turn_past_zero = (amount_turned,dir) =>
+      dial_value.isNonzero && (dir.eql("L")
         ? amount_turned >= dial_value
-        : amount_turned >= (100 - dial_value)
+        : amount_turned >= (100 - dial_value)) ? 1 : 0
 
   const update_dial = (dial_value,amount_turned,dir) =>
       (dial_value + (dir.eql("L") 
@@ -32,8 +31,7 @@ const day1 = data => {
   const total_times_past_zero = instructions.inject((sum,instruction) => {
     const [dir,steps] = [instruction.first,instruction.slice(1).to_i]
     const [full_turns,amount_turned] = steps.divmod(100)
-    sum += full_turns
-    if(part_turn_goes_past_zero(amount_turned,dir)) sum += 1
+    sum += full_turns + extra_turn_past_zero(amount_turned,dir)
     dial_value = update_dial(dial_value,amount_turned,dir)
     return sum
   },0)
