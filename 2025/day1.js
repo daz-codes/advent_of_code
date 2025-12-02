75200cb19b4517ab69434a5416f8f3d6dce27dce
DAZ <daz4126@gmail.com>
2025-12-02 19:26:46 +0000

    Update day1.js

--- 2025/day1.js
+++ 2025/day1.js
@@ -17,27 +17,22 @@ const day1 = data => {
   const instructions = data.trim().split("\n")
   let dial_value = 50
 
-  const extra_turn_past_zero = (amount_turned,dir) =>
-      dial_value.isNonzero && (dir.eql("L")
+  const part_turn_goes_past_zero = (amount_turned,dir) =>
+      dial_value.isNonzero && dir.eql("L")
         ? amount_turned >= dial_value
-        : amount_turned >= (100 - dial_value)) ? 1 : 0
-
-  const update_dial = (dial_value,amount_turned,dir) =>
-      (dial_value + (dir.eql("L") 
-         ? 100 - amount_turned 
-         : amount_turned)
-      ).modulo(100)
+        : amount_turned >= (100 - dial_value)
 
   const total_times_past_zero = instructions.inject((sum,instruction) => {
     const [dir,steps] = [instruction.first,instruction.slice(1).to_i]
     const [full_turns,amount_turned] = steps.divmod(100)
-    sum += full_turns + extra_turn_past_zero(amount_turned,dir)
-    dial_value = update_dial(dial_value,amount_turned,dir)
+    sum += full_turns
+    if(part_turn_goes_past_zero(amount_turned,dir)) sum += 1
+    dial_value += dir.eql("L") ? 100 - amount_turned : amount_turned
+    dial_value %= 100
     return sum
   },0)
   
   return total_times_past_zero
 }
-
 
 console.log(day1(data))
