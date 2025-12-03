e5672cf73223e2b18fcaa8465a38573e3f24afcb
DAZ <daz4126@gmail.com>
2025-12-03 15:29:53 +0000

    Update day3.js

--- 2025/day3.js
+++ 2025/day3.js
@@ -11,14 +11,14 @@ const NUMBER_OF_BATTERIES = 12
 
 const day3 = data => {
   const banks = data.trim().split("\n").map(x => x.split``)
-  const joltage = banks.inject((sum,bank) =>{
+  const joltage = banks.inject((sum,batteries) =>{
     const subtotal = NUMBER_OF_BATTERIES.pred.downto(1).inject((subtotal,n) => {
-      const max = bank.slice(0,-n).max
-      const index = bank.findIndex($`eql(max)`)
-      bank.splice(0,index.next)
+      const max = batteries.slice(0,-n).max
+      const index = batteries.findIndex($`eql(max)`)
+      batteries.splice(0,index.next)
       return subtotal + max * 10 ** n
     },0)
-    return sum + subtotal + bank.max
+    return sum + subtotal + batteries.max
   },0)
   
   return joltage
